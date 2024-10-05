f42e8555ad6fa76d507a6182f78bb9f166b4755e
pratikdandwate <pratikd7150@gmail.com>
2024-10-05 12:39:37 +0000

    Added all the pages required

--- app.py
+++ app.py
@@ -4,9 +4,28 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def home():
   return render_template('home.html')
 
+@app.route('/services')
+def services():
+  return render_template('services.html')
+
+@app.route('/book')
+def book():
+  return render_template('book.html')
+
+@app.route('/about-us')
+def aboutus():
+  return render_template('aboutus.html')
+
+@app.route('/contact-us')
+def contactus():
+  return render_template('contactus.html')
+
+@app.route('/login')
+def login():
+  return render_template('login.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
